0b65bc40b98aacc2f68b056ebb44b180af046b04
OrionAlzate <orion_alzate@hotmail.com>
2023-02-14 00:32:10 -0500

    clase 13 febrero 2023

---
+++ funciones.js
@@ -0,0 +1,157 @@
+//declarar variables globales
+var imagenes = [
+  {
+    nombre: "img1",
+    ruta: "imagenes/img1.jpg",
+  },
+  {
+    nombre: "img2",
+    ruta: "imagenes/img2.PNG",
+  },
+  {
+    nombre: "img3",
+    ruta: "imagenes/img3.PNG",
+  },
+  {
+    nombre: "img4",
+    ruta: "imagenes/img4.PNG",
+  },
+  {
+    nombre: "img5",
+    ruta: "imagenes/img5.PNG",
+  },
+  {
+    nombre: "img6",
+    ruta: "imagenes/img6.PNG",
+  },
+  {
+    nombre: "img1",
+    ruta: "imagenes/img1.jpg",
+  },
+  {
+    nombre: "img2",
+    ruta: "imagenes/img2.PNG",
+  },
+  {
+    nombre: "img3",
+    ruta: "imagenes/img3.PNG",
+  },
+  {
+    nombre: "img4",
+    ruta: "imagenes/img4.PNG",
+  },
+  {
+    nombre: "img5",
+    ruta: "imagenes/img5.PNG",
+  },
+  {
+    nombre: "img6",
+    ruta: "imagenes/img6.PNG",
+  },
+];
+// crear el tablero donde iran las imagenes
+var tablero = document.querySelector(".tablero");
+//variables para guardar los datos de la imagen escogida
+let imgEscogidaID = [];
+let imgEscogidaNombre = [];
+
+//variable contadora de aciertos
+let aciertos = document.querySelector(".aciertos");
+let contador = 0;
+
+// agregar imagenes al tablero por js
+function agregarImagenes() {
+  for (let i = 0; i < imagenes.length; i++) {
+    let img = document.createElement("img");
+    let div = document.createElement("div");
+    div.setAttribute("class", "col-3 border");
+    img.setAttribute("class", "img-fluid");
+    img.setAttribute("src", "imagenes/interrogacion.png");
+    img.setAttribute("id", i);
+    img.addEventListener("click", descubrirImagen);
+    div.appendChild(img);
+    tablero.appendChild(div);
+  }
+}
+agregarImagenes();
+
+// funcion para descubrir las imagenes (darle vuelta)
+
+function descubrirImagen() {
+  // this se refiere a la imagen que le demos click, vamos a tener un evento
+  let imgID = this.getAttribute("id");
+  // alert('imagen #'+imgID);
+  this.setAttribute("src", imagenes[imgID].ruta);
+  //guardar ID;
+  imgEscogidaID.push(imgID);
+  // Guardar nombre
+  imgEscogidaNombre.push(imagenes[imgID].nombre);
+  // verificar el dato que recogemos
+  // alert("id: "+imgEscogidaID[0]+", Nombre: " + imgEscogidaNombre[0]);
+  if (imgEscogidaNombre.length == 2) {
+    setTimeout(compararImagenes, 300);
+  }
+}
+
+function compararImagenes() {
+  let TodasImagenes = document.querySelectorAll(".tablero > div > img");
+  // comparar imagenes escogidas
+  if (imgEscogidaNombre[0] == imgEscogidaNombre[1]) {
+    //validar que no escojan la misma imagen
+    if (imgEscogidaID[0] == imgEscogidaID[1]) {
+      alert("Debe escoger imagenes diferentes");
+
+      TodasImagenes[imgEscogidaID[0]].setAttribute(
+        "src",
+        "imagenes/interrogacion.png"
+      );
+      TodasImagenes[imgEscogidaID[1]].setAttribute(
+        "src",
+        "imagenes/interrogacion.png"
+      );
+    } else {
+      // removeEventListener a las img
+
+      alert(`Acertaste!`);
+
+      TodasImagenes[imgEscogidaID[0]].setAttribute(
+        "src",
+        "imagenes/acierto.png"
+      );
+      TodasImagenes[imgEscogidaID[1]].setAttribute(
+        "src",
+        "imagenes/acierto.png"
+      );
+      TodasImagenes[imgEscogidaID[0]].removeEventListener(
+        "click",
+        descubrirImagen,
+        false
+      );
+      TodasImagenes[imgEscogidaID[1]].removeEventListener(
+        "click",
+        descubrirImagen,
+        false
+      );
+      contador++;
+      console.log(contador);
+      aciertos.textContent = contador;
+
+      if (contador >= 6) {
+        alert("Ganaste!!!!!!!!!!!!");
+      }
+    }
+  } else {
+    alert("Son diferentes");
+    TodasImagenes[imgEscogidaID[0]].setAttribute(
+      "src",
+      "imagenes/interrogacion.png"
+    );
+    TodasImagenes[imgEscogidaID[1]].setAttribute(
+      "src",
+      "imagenes/interrogacion.png"
+    );
+  }
+  // reiniciar la variable
+  imgEscogidaNombre = [];
+  imgEscogidaID = [];
+}
